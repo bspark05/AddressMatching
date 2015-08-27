40d4a3a0259dcfbfa69225825e794d8ca1b2e791
bspark05 <bpark21@asu.edu>
2015-08-26 18:33:27 -0700

    08262015

---
+++ Juso/Seoul.py
@@ -0,0 +1,8 @@
+'''
+Created on Aug 23, 2015
+
+@author: Administrator
+'''
+
+def findBuildingMGT(addr1, addr2, addr3, addr4):
+    return
---
+++ Main.py
@@ -0,0 +1,8 @@
+'''
+Created on Aug 23, 2015
+
+@author: Administrator
+'''
+
+if __name__ == '__main__':
+    
