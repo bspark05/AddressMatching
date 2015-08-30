4dbc674af0498cd49f3108f260fe0281d3c870fb
bspark05 <bpark21@asu.edu>
2015-08-29 23:33:29 -0700

    08292015-1

--- Main.py
+++ Main.py
@@ -15,48 +15,48 @@ if __name__ == '__main__':
 #     filename='Dictionary'
 #     filepath = filename+'.xlsx'
 #     sheetname = 'Sheet1'
-#     
+#      
 #     dicResult = excel.excelRead(filepath, sheetname)
-#     
+#      
 #     newDic = []
 #     index1 = 0
 #     index2 = 0
-#     
-#     
+#      
+#      
 #     for dicRow in dicResult[1:] :
 #         addr1 = int(str(dicRow[4].value)[:5])
 #         addr2 = int(str(dicRow[4].value)[5:8])
 #         addr3 = int(dicRow[1].value)
 #         addr4 = int(dicRow[2].value)
-#         
+#          
 #         matchingResult = seoul.findBuildingInfo(addr1, addr2, addr3, addr4)
-#         
+#          
 #         dicRow.append(matchingResult[0])
 #         dicRow.append(matchingResult[1])
-#         
+#          
 #         #print(matchingResult[1])
-#         
-#         
+#          
+#          
 #         if matchingResult[1] == '0.0' or matchingResult[1] == '-1':
 #             dicRow.append(matchingResult[2])
 #             dicRow.append(matchingResult[3])
-#             
+#              
 #             index1 += 1
-#             
+#              
 #         else:
 #             locationEQB = seoul.findEQBInfo(addr1, matchingResult[1])
 #             dicRow.append(locationEQB[0])
 #             dicRow.append(locationEQB[1])
-#             
+#              
 #             index2 += 1
-#         
+#          
 #         newDic.append(dicRow)
-#     
-#     
+#      
+#      
 #     print("index1= "+str(index1))
 #     print("index2= "+str(index2))
-#     
-#     
+#      
+#      
 #     newFilename = filename+'_result.xlsx'
 #     newSheetname = 'Sheet2'    
 #     excel.excelWriteNewFile(newFilename, newSheetname, newDic)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     filenameRem = 'Dictionary_result.xlsx'
     sheetnameRem = 'Sheet1'
     remainDic = excel.excelRead(filenameRem, sheetnameRem)
-    
+     
     geocodingResult = geocode.geocodeList(remainDic)
-    
+     
     excel.excelWriteOnExistingFile(filenameRem, sheetnameRem, "H", geocodingResult)
